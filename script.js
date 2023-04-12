cc84f668d2c20d814ff151fe4d7cb0a7559d35d0
Erick384 <erickwachuma@gmail.com>
2023-04-12 23:57:44 +0300

    Order Summary commits

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-let element = document.querySelector('.two');
-let detail = document.querySelector('#change');
+let element = document.querySelector(".two");
+let detail = document.querySelector("#change");
 
-detail.addEventListener('click', function() {
-  // Update the detail text
-  element.textContent = 'No other Plan!';
+let payment = document.querySelector(".button");
+let bottom = document.querySelector(".bottom");
+payment.addEventListener("click", function () {
+  bottom.textContent = "Order Placed Successfully!!";
 });
 
-let payment = document.querySelector('.button');
-let bottom = document.querySelector('.bottom')
-payment.addEventListener('click', function(){
-    bottom.textContent = 'Order Placed Successfully!!'
-}
-)
+let options = document.querySelector("#my-options");
+let optionDivs = options.querySelectorAll(".option");
+let output = document.querySelector("#output");
+
+detail.addEventListener("click", function () {
+  // Show the options
+  options.style.display = "block";
+  // Add event listener to each option div
+  optionDivs.forEach(function (optionDiv) {
+    optionDiv.addEventListener("click", function () {
+      // Choose the selected option
+      output.innerHTML = optionDiv.innerHTML;
+      // Hide the options
+      options.style.display = "none";
+    });
+  });
+});
